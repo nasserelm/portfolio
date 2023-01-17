1656889f89c000cfd3a3308f5246314f26e754b2
Nasser Mughrabi <abdalnasseralmughrabi@gmail.com>
2023-01-17 14:39:52 -0700

    changed about image

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components/macro";
-import myImg from '../img/about-image1.png';
+import myImg from "../img/nassers.jpg";
 
 const ResumeContainer = styled.a`
   padding: 1rem 0rem;
@@ -24,15 +24,15 @@ const ImageContainer = styled.div`
   background-size: cover;
   background-position: 60% 40%;
   background-repeat: no-repeat;
-  // filter: brightness(170%); 
+  // filter: brightness(170%);
   @media (max-width: 1024px) {
-    display:none;
+    display: none;
   }
-`
+`;
 
 const Description = styled.div`
   color: white;
-  padding: 5% 10% ;
+  padding: 5% 10%;
   display: flex;
   flex-direction: column;
   align-items: left;
@@ -44,12 +44,12 @@ const Description = styled.div`
     padding: 1rem 0rem;
     line-height: 1.2rem;
     font-size: 1rem;
-    @media (min-width: 800px){
+    @media (min-width: 800px) {
       font-size: 2.5vmin;
-      line-height:5vmin;
+      line-height: 5vmin;
     }
   }
-`
+`;
 
 const PageWrapper = styled.div`
   height: calc(100vh - 3rem);
@@ -91,20 +91,20 @@ function About() {
           </ResumeContainer>
         </h2>
         <h4>
-          Current computer science student at the University of Utah.
-          Passionate about programming and solving problems using the most
-          efficient algorithms and data structures accessible.
+          Current computer science student at the University of Utah. Passionate
+          about programming and solving problems using the most efficient
+          algorithms and data structures accessible.
           <br></br>
-          For the past two years, I have been working on Full Stack
-          Developement projects. In this time, I have gained extensive
-          experience in both front-end and back-end development. I am confident
-          in my ability to create high-quality and well-functioning web
-          applications. I am also experienced in using various programming
-          languages and frameworks, such as React, Django, Node.js, and more.
+          For the past two years, I have been working on Full Stack Developement
+          projects. In this time, I have gained extensive experience in both
+          front-end and back-end development. I am confident in my ability to
+          create high-quality and well-functioning web applications. I am also
+          experienced in using various programming languages and frameworks,
+          such as React, Django, Node.js, and more.
           <br></br>
           Working on team and personal projects helped put Full Stack
           development skills, problem-solving techniques, and teamwork skills
-          into practice. 
+          into practice.
         </h4>
       </Description>
     </PageWrapper>
