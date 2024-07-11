7df7bd5dbe9e2baa25fd8d7dc924890c9b41d845
Nasser Mughrabi <nasser@Nassers-MacBook-Air.local>
2024-07-11 10:27:35 -0600

    updated realestate project tags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled, { createGlobalStyle } from "styled-components/macro";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import NavBar from "./components/NavBar";
+import emailjs from "emailjs-com";
 
 // Sets up global styling
 const GlobalStyle = createGlobalStyle`
@@ -26,6 +27,32 @@ const FullPage = styled.div`
 `;
 
 function App() {
+  useEffect(() => {
+    console.log(window.location.pathname, window.location.search);
+
+    const templateParams = {
+      message:
+        "Someone visited your website from: " +
+        window.location.pathname +
+        window.location.search,
+    };
+    emailjs
+      .send(
+        "service_qx3lqbp",
+        "template_h7bivnl",
+        templateParams,
+        "01pNSYIZiCoEt2dVi"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }, []);
+
   return (
     <React.Fragment>
       <GlobalStyle />
