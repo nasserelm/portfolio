1ba22279b81e39ff065d370a24ac3daff912595e
Nasser Mughrabi <nasser@Nassers-MacBook-Air.local>
2024-07-20 15:21:53 -0600

    updated about and projects order

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,6 @@ const ImageContainer = styled.div`
   background-size: cover;
   background-position: 60% 40%;
   background-repeat: no-repeat;
-  // filter: brightness(170%);
   @media (max-width: 796px) {
     display: none;
   }
@@ -37,18 +36,25 @@ const Description = styled.div`
   display: flex;
   flex-direction: column;
   align-items: left;
+  font-family: "Raleway", sans-serif;
   h2 {
     width: fit-content;
   }
   h4 {
-    font-family: "Raleway", sans-serif;
     padding: 1rem 0rem;
-    line-height: 1.2rem;
-    font-size: 1rem;
+    line-height: 1.5rem;
+    font-size: 1.2rem;
     @media (min-width: 1023px) {
-      //800px
       font-size: 2.5vmin;
       line-height: 5vmin;
+    }
+  }
+  p {
+    margin-top: 1rem;
+    line-height: 1.5;
+    font-size: 1.1rem;
+    @media (min-width: 1023px) {
+      font-size: 2vmin;
     }
   }
 `;
@@ -56,7 +62,6 @@ const Description = styled.div`
 const PageWrapper = styled.div`
   height: calc(100vh - 3rem);
   overflow: hidden;
-  // background-image: linear-gradient(335deg,#003e61,#1e2f59 90%,#ffc700);
   background-color: #1c1c21;
   display: grid;
   grid-template-columns: 1fr 1fr;
@@ -65,12 +70,6 @@ const PageWrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    /* background-image: linear-gradient(
-        to bottom,
-        rgba(0, 0, 0, 0.3),
-        rgba(0, 0, 0, 0.3)
-      ),
-      url(${myImg}); */
     background-size: cover;
     background-position: 40% 10%;
     text-shadow: 1px 1px black;
@@ -79,7 +78,6 @@ const PageWrapper = styled.div`
 `;
 
 const Button = styled.a`
-  /* Button styles */
   padding: 10px 20px;
   font-size: 16px;
   background-color: #2196f3;
@@ -91,13 +89,9 @@ const Button = styled.a`
   font-size: 24px;
   font-weight: bold;
   text-align: center;
-
-  /* Hover styles */
   &:hover {
     background-color: #0d8bf0;
   }
-
-  /* Disabled styles */
   &:disabled {
     background-color: #ccc;
     cursor: not-allowed;
@@ -107,7 +101,7 @@ const Button = styled.a`
 function About() {
   return (
     <PageWrapper id="about">
-      <ImageContainer></ImageContainer>
+      <ImageContainer />
       <Description>
         <h2>
           <ResumeContainer
@@ -119,49 +113,28 @@ function About() {
           </ResumeContainer>
         </h2>
         <p>
-          <br></br>
-          <br></br>
-          Former Software Engineer Intern at Amotions Inc. with Bachelor's in
-          Computer Science from the University of Utah
-          <br></br>
-          <br></br>A dedicated software engineer who effortlessly navigates the
-          ever-changing landscape of technology, fearlessly tackling complex
-          problems and seamlessly collaborating with diverse teams. Always ready
-          to embark on the next exciting challenge.
-          <br></br>
-          <br></br>
-          During my internship at Amotions Inc. I worked on Amotions' services
-          utilizing different technologies and programming languages. I
-          collaborated with a team of talented Software Engineers and Designers
-          to build functional and appealing user interfaces utilizing
-          React/Next.js. I also led a team of software engineers to lauched a
-          new trained AI product that resembled a human coach helping clients
-          grow professionaly and improve interactions with thier clients and
-          other employees in the workplace.
-          {/* <br></br>
-          <br></br>
-          Programming Languages: Java, C#, JavaScript, TypeScript, Python
-          <br></br>
-          <br></br>
-          Frontend: HTML/CSS, React, Vue.js, Next.js
-          <br></br>
-          <br></br>
-          Database: SQL, MySQL, MongoDB
-          <br></br>
-          <br></br>
-          Backend: Spring Boot, .NET, Node.js, Flask, REST APIs
-          <br></br>
-          <br></br>
-          Other: Unit Testing, Selenium, AWS, Git, GitHub, GitLab */}
+          I am a part-time Software Engineer at Amotions Inc., a startup in
+          California focused on enhancing professional growth and interpersonal
+          interactions. Additionally, I provide freelance software development
+          services to various businesses across Utah and the United States.
         </p>
-
-        {/* <Button
-          href={process.env.PUBLIC_URL + "Nasser_Mughrabi_resume.pdf"}
-          style={{ textDecoration: "none", color: "inherit" }}
-          target="_blank"
-        >
-          Resume
-        </Button> */}
+        <p>
+          I hold a Bachelor's in Computer Science from the University of Utah
+          and an Associate of Science from Snow College.
+        </p>
+        <p>
+          As a dedicated Software Engineer, I navigate the ever-evolving tech
+          landscape, solving complex problems and collaborating with diverse
+          teams. Always ready for the next exciting challenge.
+        </p>
+        <p>
+          At Amotions Inc., I work on a variety of services using different
+          technologies and programming languages. I collaborate with talented
+          Software Engineers and Designers to create functional and appealing
+          user interfaces using React/Next.js. I also led a team to launch a new
+          AI product that acts as a human coach, helping clients grow
+          professionally and improve their workplace interactions.
+        </p>
       </Description>
     </PageWrapper>
   );
